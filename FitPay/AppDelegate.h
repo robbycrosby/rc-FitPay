02c9104a8c4c60c59661aa072f338107d3c9cb4d
Robert Crosby <Rob@Roberts-MacBook-Air.local>
2015-04-05 10:36:26 -0700

    Initial Commit

---
+++ FitPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitPay
+//
+//  Created by Robert Crosby on 4/5/15.
+//  Copyright (c) 2015 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FitPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FitPay
+//
+//  Created by Robert Crosby on 4/5/15.
+//  Copyright (c) 2015 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
